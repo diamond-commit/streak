f1e60d5da0253e8c76856e27b80c5ee5dd64ace9
fawas diamond <fawasdiamond2007@gmail.com>
2025-01-29 02:44:39 +0100

    Was def not easy

--- script.js
+++ script.js
@@ -2,9 +2,24 @@
    const button = document.getElementById("button")
    const container = document.getElementById("contain")
     button.addEventListener ("click", function () {
+      button.remove()
+      const text = document.createElement("h2")
+      container.appendChild(text)
+      text.textContent = "200 streak🔥! Too easy 🕺"
+      const photo = document.createElement("div")
+      photo.classList.add("photo")
+      container.appendChild(photo)
+      
+
+    })
+    function createPhoto (){
         const photo = document.createElement("div")
         photo.classList.add("photo")
         container.appendChild(photo)
         button.remove()
-    })
+    }
+     function createText (){
+        const text = document.createElement("h2")
+        container.appendChild(text)
+     }
 
