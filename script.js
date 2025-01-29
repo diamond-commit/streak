809f9398b640ce16f24ea240d40d677c6587d32c
fawas diamond <fawasdiamond2007@gmail.com>
2025-01-29 01:06:51 +0100

    added asap

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+  console.log("page loaded")
+   const button = document.getElementById("button")
+   const container = document.getElementById("contain")
+    button.addEventListener ("click", function () {
+        const photo = document.createElement("div")
+        photo.classList.add("photo")
+        container.appendChild(photo)
+        button.remove()
+    })
+
